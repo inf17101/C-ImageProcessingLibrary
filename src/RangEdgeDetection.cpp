e175ecce19e64dc2db23fcac728ae94a88bee808
Oliver Klapper <oliver.klapper@web.de>
2020-05-27 21:41:17 +0200

    implements part of RangEdgeDetection hard coded

--- src/RangEdgeDetection.cpp
+++ src/RangEdgeDetection.cpp
@@ -2,6 +2,7 @@
 #include "Sobel.h"
 #include <memory>
 #include <map>
+#include <algorithm>
 
 std::unique_ptr<PicturePGM> RangEdgeDetection::make_padding(PicturePGM* pic)
 {
@@ -21,6 +22,11 @@ std::unique_ptr<PicturePGM> RangEdgeDetection::make_padding(PicturePGM* pic)
         for(int j=0; j<new_pic->width; ++j)
             new_pic->map[i][j] = 0;
     }
+
+    for(int i=1; i<new_pic->height-1; ++i)
+        for(int j=1; j<new_pic->width-1; ++j)
+            new_pic->map[i][j] = pic->map[i-1][j-1];
+
     return new_pic;
 }
 
@@ -33,11 +39,23 @@ std::unique_ptr<PicturePGM> RangEdgeDetection::processImage(PicturePGM* pic, Con
     delete c_sobel["gradient_only"];
     auto GradientPicture_WithPadding = make_padding(GradientPicture.get());
 
+    int pixel_surrounding[9];
+
     for(int row=0; row<GradientPicture_WithPadding->height-2; row++)
     {
         for(int col=0; col<GradientPicture_WithPadding->width-2; col++)
         {
+            for(int i=0; i<3; ++i)
+                for(int j=0; j<3; ++j)
+                    pixel_surrounding[i*3 + j] = GradientPicture_WithPadding->map[row+i][col+j];
+            
+            std::sort(std::begin(pixel_surrounding), std::end(pixel_surrounding));
+            for(int k=0; k<9; ++k)
+                printf("%d ", pixel_surrounding[k]);
+            
+            printf("\n");
             
         }
     }
+    return GradientPicture_WithPadding;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,20 @@
 #include "ImageAlgorithmStrategy.h"
 #include "ImageConfig.h"
 #include "ImageProcessor.h"
+#include "RangEdgeDetection.h"
 
 int main()
 {
+    /*
     const char FILE_PATH[] = "images/SteilkurveGRAY_pgm.pgm";
     auto imProcessor = std::make_unique<ImageProcessor>(std::make_unique<Sobel>());
+    */
+    const char FILE_PATH[] = "images/TestSimple9x9.pgm";
+    auto imProcessor = std::make_unique<ImageProcessor>(std::make_unique<RangEdgeDetection>());
     auto picture = imProcessor->readImage(FILE_PATH);
 
     Config c;
+    /*
     c["sobel_threshold"] =  new TypedImageProperty<float>("sobel_threshold", 100.0);
     c["gradient_only"] = new TypedImageProperty<bool>("gradient_only", false);
     auto sobel_pic = imProcessor->processImage(picture.get(), c);
@@ -24,7 +30,10 @@ int main()
 
     for(auto it = c.begin(); it != c.end(); ++it)
         delete (it->second);
+    */
 
+    auto output_pic = imProcessor->processImage(picture.get(), c);
+    output_pic->printPic();
     return 0;
 }
 
